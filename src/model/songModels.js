c8442e285aa47fb621d315f7dbe6f909a3e220da
Carolain Seija <carolainsilva1@gmail.com>
2020-09-23 01:47:57 -0300

    songModel de prueba

---
+++ src/model/songModels.js
@@ -0,0 +1,33 @@
+//MONGOOSE
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+//Coneccion
+mongoose.connect(
+  "mongodb+srv://carolain-seija:crosiljs1@cluster0.dk3mk.mongodb.net/proyectotres?retryWrites=true&w=majority",
+  { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false }
+);
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", function () {
+  console.log("MongoDB Connected successfully");
+});
+
+//ESQUEMA
+let songSchema = new Schema({
+  name: { type: String },
+  artist: { type: String },
+});
+//MODELO
+let Song = mongoose.model("Song", songSchema);
+
+//FUNCIONES
+
+const allSongsproyect = async () => {
+  const theSongs = await Song.find();
+  return theSongs;
+};
+
+module.exports = {
+    allSongsproyect
+};
