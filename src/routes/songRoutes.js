fabf5e57877e9a9c12f53a9e15076a6a2b1dc68b
Carolain Seija <carolainsilva1@gmail.com>
2020-09-23 01:46:13 -0300

    songRoutes de prueba
    esto es para verificar que me conecto con exito a la base de datos

--- src/routes/songRoutes.js
+++ src/routes/songRoutes.js
@@ -1,14 +1,24 @@
+//RUTAS - EXPRESS
 const express = require("express");
-
+const controller = require('../controller/songController');
 
 var app = express();
 app.use(express.json());
 
 
-//get devuelve todo los usuarios
+//get devuelve toda las canciones
 app.route("/")
   .get(async(req, res) => {
-    res.send('todo ok');
+    try{
+      const canciones = await controller.allSongs();
+      if(canciones.length > 0) {
+          res.status(200);
+        res.send(canciones)
+      }
+      res.status(404).send("no se pudo encontrar nada");
+    } catch(e) {
+      throw e;
+    }
   })
 
 
